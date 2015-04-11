386d48bccc3f835fcc2e8e8253200770384446f5
E231moha <kame.setuna@gmail.com>
2015-04-12 02:04:20 +0900

    neko4を実施

--- Practice/Practice/Source.cpp
+++ Practice/Practice/Source.cpp
@@ -1,40 +1,39 @@
-//omikuji3.cpp
 #include <iostream>
-#include <cstdlib>
-#include <ctime>
+#include <string>
 using namespace std;
 
-class Omikuji {
-	int un;  //ラッキーナンバー
+class Cat {
+	string name;
+	int Hitpoint;
 public:
-	Omikuji();   //コンストラクタ
-	void hiku();
+	Cat(string);
+	int eat();
+	int naku();
 };
 
-//コンストラクタ、ラッキーナンバーをユーザに代入してもらいます
-Omikuji::Omikuji() {
-	srand((unsigned)time(NULL));
-	cout << "自分のラッキーナンバー（1～5）を入力してください。" << endl;
-	cin >> un;  //自分のラッキーナンバーを代入
+Cat::Cat(string n) {
+	name = n;
+	Hitpoint = 10;
 }
 
-//おみくじを引く
-void Omikuji::hiku() {
+int Cat::eat() {
+	cout << "猫に与える食事の量を入力してください。" << endl;
 	int x;
-	x = rand() % 5 + 1;   //1～5の乱数を発生させ、ｘに代入、これが引いたおみくじの番号
-	cout << "あなたの運勢は";
-	if(x == un) {                                            //xとunが等しければ大吉
-		cout << "大吉です。" << endl;
-	} else {                                                    //xとunが等しくなければ「普通」
-		cout << "吉です。" << endl;
-	}
+	cin >> x;
+	Hitpoint -= x;
+	cout << "猫は食事をしました。" << endl;
+	return Hitpoint;
+}
+
+int Cat::naku() {
+	cout << "猫は鳴きました。" << name << endl;
+	Hitpoint -= 5;
+	return Hitpoint;
 }
 
 int main() {
-	Omikuji ok;  //おみくじ
-	cout << "1週間分の占いです。" << endl;
-	for(int i = 0; i < 7; i++) {
-		cout << "今日から" << i + 1 << "日後：" << endl;
-		ok.hiku();
-	}
+	Cat neko("dora");
+	neko.eat();
+	neko.naku();
+	return 0;
 }
