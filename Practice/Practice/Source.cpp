1fb8640cca303f318d04f75670ea395fc3174a0a
E231moha <kame.setuna@gmail.com>
2015-04-12 01:35:34 +0900

    おみくじ

--- Practice/Practice/Source.cpp
+++ Practice/Practice/Source.cpp
@@ -1,12 +1,40 @@
-//while_sample.cpp
+//omikuji3.cpp
 #include <iostream>
+#include <cstdlib>
+#include <ctime>
 using namespace std;
 
+class Omikuji {
+	int un;  //ラッキーナンバー
+public:
+	Omikuji();   //コンストラクタ
+	void hiku();
+};
+
+//コンストラクタ、ラッキーナンバーをユーザに代入してもらいます
+Omikuji::Omikuji() {
+	srand((unsigned)time(NULL));
+	cout << "自分のラッキーナンバー（1～5）を入力してください。" << endl;
+	cin >> un;  //自分のラッキーナンバーを代入
+}
+
+//おみくじを引く
+void Omikuji::hiku() {
+	int x;
+	x = rand() % 5 + 1;   //1～5の乱数を発生させ、ｘに代入、これが引いたおみくじの番号
+	cout << "あなたの運勢は";
+	if(x == un) {                                            //xとunが等しければ大吉
+		cout << "大吉です。" << endl;
+	} else {                                                    //xとunが等しくなければ「普通」
+		cout << "吉です。" << endl;
+	}
+}
+
 int main() {
-	int i;
-	i = 0;
-	while(i < 100) {
-		cout << i << "  "; //見やすさのため数字の後に空白をいれた
-		i++;                   //iを１増やせという意味です。（下を参照）
+	Omikuji ok;  //おみくじ
+	cout << "1週間分の占いです。" << endl;
+	for(int i = 0; i < 7; i++) {
+		cout << "今日から" << i << "日後：" << endl;
+		ok.hiku();
 	}
 }
