7d875554d81b392e9c0e6e8208c7101cb8b65566
E231moha <kame.setuna@gmail.com>
2015-04-12 01:19:30 +0900

    whileループ

--- Practice/Practice/Source.cpp
+++ Practice/Practice/Source.cpp
@@ -1,7 +1,12 @@
+//while_sample.cpp
 #include <iostream>
 using namespace std;
 
 int main() {
-	cout << "やぁ、こんちは。" << endl;
-	return 0;
+	int i;
+	i = 0;
+	while(i < 100) {
+		cout << i << "  "; //見やすさのため数字の後に空白をいれた
+		i++;                   //iを１増やせという意味です。（下を参照）
+	}
 }
