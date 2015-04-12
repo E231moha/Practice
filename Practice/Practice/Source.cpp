c54a2296f94cc338ceb1dca380c06dc19bba6a27
E231moha <kame.setuna@gmail.com>
2015-04-12 20:15:07 +0900

    new,delete演算子

--- Practice/Practice/Source.cpp
+++ Practice/Practice/Source.cpp
@@ -1,27 +1,48 @@
-﻿//dest_sample.cpp
+﻿//gakusei_sample2.cpp
 #include <iostream>
 using namespace std;
 
-class Nanika {
-	int datum;
+class Gakusei {
+	int sansu;  //算数の点
 public:
-	//コンストラクタ 
-	Nanika(int x): datum(x) {
-		cout << "Nanikaのインスタンス" << datum << "が生成されました。" << endl;
-	}
-	void func() const {
-		cout << "Nanikaのインスタンス" << datum << "のfuncが呼ばれました。" << endl;
-	}
-	//デストラクタ 
-	~Nanika() {
-		cout << "Nanikaのインスタンス" << datum << "が消滅しました。" << endl;
-	}
+	//引数なしのコンストラクタ（これがないとプログラムはエラー）
+	Gakusei() {}  //何もしないので、中カッコ内は空
+	//引数ありのコンストラクタ
+	Gakusei(int x) { set_sansu(x); }
+	void set_sansu(int x);
+	int get_sansu() const { return sansu; }
+	void input();    //入力のために新しく付けた
 };
 
-int main() {
-	//プログラマがはじめから数値1、2を与えてインスタンスOne、Twoをつくる
-	Nanika One(1), Two(2);
+void Gakusei::set_sansu(int x) {
+	if(x >= 100) {
+		//xが100以上の時は、100を代入してしまう。
+		x = 100;
+	}
+	sansu = x;
+}
 
-	One.func();
-	Two.func();
+void Gakusei::input() {
+	int temp;
+	cout << "算数の点を入力してください：";
+	cin >> temp;
+	set_sansu(temp);
+}
+
+int main() {
+	int ninzu, sum = 0;
+	Gakusei *pm;
+	cout << "学生の人数を入力してください。" << endl;
+	cin >> ninzu;
+	pm = new Gakusei[ninzu];   //引数を取らないコンストラクタが使われる 
+	cout << ninzu << "人の学生の点数を入力してください。" << endl;
+	for(int i = 0; i < ninzu; i++) {
+		pm[i].input();
+	}
+	cout << "それでは平均を計算します。" << endl;
+	for(int i = 0; i < ninzu; i++) {
+		sum += pm[i].get_sansu();
+	}
+	cout << "平均点は" << sum / ninzu << "です。" << endl;
+	delete [] pm;
 }
